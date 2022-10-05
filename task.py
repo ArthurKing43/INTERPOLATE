d2a55012eaed25528a5caaa8a41f15a5e360d048
ArthurKing43 <art.akhmedov2013@yandex.ru>
2022-10-05 19:26:00 +0300

    Task_1 complete Исправлена проблема с численными переменными

--- task.py
+++ task.py
@@ -1,15 +1,19 @@
 N = open('NuMBeRS', 'r')
-List = N.readline()
-List_1 = List.split(' ')
-print(List_1)
-for i in range(0, len(List_1) - 1):
-    if List_1[i + 1] <= List_1[i]:
+S = N.readline()
+S_1 = S.split(' ')
+S_2 = []
+for char in S_1:
+     x = int(char)
+     S_2.append(x)
+
+for i in range(0, len(S_2) - 1):
+    if S_2[i + 1] <= S_2[i]:
         for k in range(i, -1, -1):
-            if List_1[k + 1] <= List_1[k]:
-                t = List_1[k + 1]
-                List_1[k + 1] = List_1[k]
-                List_1[k] = t
-print(List_1)
+            if S_2[k + 1] <= S_2[k]:
+                t = S_2[k + 1]
+                S_2[k + 1] = S_2[k]
+                S_2[k] = t
+print(S_2)
 '''3/ 8 2 1 4 i=0
 
 3 8/ 2 1 4 i=1
